7a68a9e7c33bd7fcf418ccc11c40764c9fa79eee
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-07-21 01:14:58 +0500

    Added the Menus dynamically

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import Categories from './Categories';
+import items from './data';
 
 const Menu = () => {
+  let categoryMenuArr = [];
+  items.map((catItem) => {
+    categoryMenuArr.push(catItem.category);
+  });
+  //  The following filter method will return the value only one item if it's repeated repeatedly.
+  //  The first argument will return al the values in the array one by one
+  //  The second argument will return the index
+  //  The third argument will return the original array itself
+  //  Inside the function we used indexOf to compare the currentVal with in the array by indexOf vale
+  let categoryMenus = categoryMenuArr.filter(
+    (singleValue, arrayIndex, arrayItself) => {
+      //console.log(arrayItself);
+      console.log(arrayItself.indexOf(singleValue), singleValue);
+
+      return arrayItself.indexOf(singleValue) === arrayIndex;
+    }
+  );
+
   return (
     <>
       <main>
@@ -14,15 +33,13 @@ const Menu = () => {
             <button type="button" className="filter-btn">
               all
             </button>
-            <button type="button" className="filter-btn">
-              breakfast
-            </button>
-            <button type="button" className="filter-btn">
-              lunch
-            </button>
-            <button type="button" className="filter-btn">
-              shakes
-            </button>
+            {categoryMenus.map((menu) => {
+              return (
+                <button type="button" className="filter-btn">
+                  {menu}
+                </button>
+              );
+            })}
           </div>
           <div className="section-center">
             <Categories />
