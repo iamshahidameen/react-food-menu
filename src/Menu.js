cb8c4a64a5dfbd4e7367b957e29af1bfceefda91
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-07-21 02:24:16 +0500

    used new Set() method for getting unique values from the array. This is best approach as compared to previous one because we also removed the All Button from menu which was static/hardcoded and by using array spread operator now it will be dynamic

--- src/Menu.js
+++ src/Menu.js
@@ -16,16 +16,17 @@ const Menu = () => {
     }
   }
 
-  let categoryMenuArr = [];
-
-  items.map((catItem) => {
-    categoryMenuArr.push(catItem.category);
-  });
   //  The following filter method will return the value only one item if it's repeated repeatedly.
   //  The first argument will return al the values in the array one by one
   //  The second argument will return the index
   //  The third argument will return the original array itself
   //  Inside the function we used indexOf to compare the currentVal with in the array by indexOf vale
+  //  This is old method for filter
+  /*let categoryMenuArr = [];
+
+   items.map((catItem) => {
+     categoryMenuArr.push(catItem.category);
+   });
   let categoryMenus = categoryMenuArr.filter(
     (singleValue, arrayIndex, arrayItself) => {
       //console.log(arrayItself);
@@ -33,7 +34,14 @@ const Menu = () => {
 
       return arrayItself.indexOf(singleValue) === arrayIndex;
     }
-  );
+  );*/
+  //  Below code is for getting the unique Menu items
+  //  We can also use set() dataStructure for getting unique values, and it is the best approach. we can also append "ALL" as a category menu by the help of Array spread operator, and it will return us the unique values with all added in the list
+
+  /* 
+  let categoryMenus = ['all', ...new Set(items.map((item) => item.category))]
+  */
+  const categoryMenus = ['all', ...new Set(items.map((item) => item.category))];
 
   return (
     <>
@@ -44,13 +52,6 @@ const Menu = () => {
             <div className="underline"></div>
           </div>
           <div className="btn-container">
-            <button
-              type="button"
-              className="filter-btn"
-              onClick={() => filterMenus('all')}
-            >
-              all
-            </button>
             {categoryMenus.map((menu) => {
               return (
                 <button
