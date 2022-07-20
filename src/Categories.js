3c31d71c5f6618d23d99324860d35f53731f6144
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-07-20 22:31:38 +0500

    Added layout for Food Menu

--- src/Categories.js
+++ src/Categories.js
@@ -1,7 +1,44 @@
 import React from 'react';
 
 const Categories = () => {
-  return <h2>categories component</h2>;
+  return (
+    <>
+      <article className="menu-item">
+        <img
+          src="./images/item-1.jpeg"
+          alt="buttermilk pancakes"
+          className="photo"
+        />
+        <div className="item-info">
+          <header>
+            <h4>buttermilk pancakes</h4>
+            <h4 className="price">$15</h4>
+          </header>
+          <p className="item-text">
+            I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock
+            freegan copper mug whatever cold-pressed
+          </p>
+        </div>
+      </article>
+      <article className="menu-item">
+        <img
+          src="./images/item-2.jpeg"
+          alt="buttermilk pancakes"
+          className="photo"
+        />
+        <div className="item-info">
+          <header>
+            <h4>Diner Double</h4>
+            <h4 className="price">$19</h4>
+          </header>
+          <p className="item-text">
+            vaporware iPhone mumblecore selvage raw denim slow-carb leggings
+            gochujang helvetica man braid jianbing. Marfa thundercats
+          </p>
+        </div>
+      </article>
+    </>
+  );
 };
 
 export default Categories;
--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import Categories from './Categories';
 
 const Menu = () => {
-  return <h2>menu component</h2>;
+  return (
+    <>
+      <main>
+        <section className="menu section">
+          <div className="title">
+            <h2>our menu</h2>
+            <div className="underline"></div>
+          </div>
+          <div className="btn-container">
+            <button type="button" className="filter-btn">
+              all
+            </button>
+            <button type="button" className="filter-btn">
+              breakfast
+            </button>
+            <button type="button" className="filter-btn">
+              lunch
+            </button>
+            <button type="button" className="filter-btn">
+              shakes
+            </button>
+          </div>
+          <div className="section-center">
+            <Categories />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default Menu;
